4314da3f88119f9b986160f21b7f435737124f0f
HP <radadiyakrish62@gmail.com>
2023-09-14 10:52:04 +0530

    reupload

---
+++ android/app/src/main/kotlin/com/example/e_commrce_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.e_commrce_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/global.dart
@@ -0,0 +1,290 @@
+class global {
+  static List<Map<String, dynamic>> allProducts = [
+
+    {
+      'categoryName': 'south Indian',
+      'categoryProducts': [
+        {
+          'id': 1,
+          'name': 'Dhosa',
+          'price': 250,
+          'ratting': 5,
+          'discount': 12,
+          'thumbnail':
+              "https://www.culturalindia.net/iliimages/South%20Indian%20Food_1.jpg"
+        },
+        {
+          'id': 2,
+          'name': 'idli sambhar',
+          'price': 200,
+          'ratting': 4,
+          'discount': 10,
+          'thumbnail':
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ65dimJZr2jKRLXcWuGN-vBhOeFGXfBbQSxg&usqp=CAU",
+        },
+        {
+          'id': 3,
+          'name': 'medu vada',
+          'price': 170,
+          'ratting': 3,
+          'discount': 17,
+          'thumbnail':
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-KriQmCvAEy2Su1BZPXp7kjrPOSdT3cZlqg&usqp=CAU",
+        },
+        {
+          'id': 4,
+          'name': 'Masala Dhosa ',
+          'price': 400,
+          'ratting': 4.78,
+          'discount': 14,
+          'thumbnail':
+              "https://img.traveltriangle.com/blog/wp-content/uploads/2022/01/Famous-Food-Of-South-India.jpg"
+        },
+        {
+          'id': 5,
+          'name': 'appam',
+          'price': 250,
+          'ratting': 4.9,
+          'discount': 12,
+          'thumbnail':
+              "https://www.jeyashriskitchen.com/wp-content/uploads/2021/08/IMG_0640.jpg"
+        },
+      ],
+    },
+    {
+      'categoryName': 'Panjabi',
+      'categoryProducts': [
+        {
+          'id': 1,
+          'name': 'Tawa Kulcha',
+          'price': 360,
+          'ratting': 4,
+          'discount': 10,
+          'thumbnail':
+              "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjr4iyPWqMMOLkOqrvT_K2LrEFGpkNFkaib3z4U_3GKxK2-1OTGXPtFQQvaouXmGXWkaw5Ynzq2os7ZGrChAeWBd1Ev4tjz26nnWR6YBqDD-wt4lTb4ZS16LB1RyFyPN9Ij_9KsRjGPOGO4Rh3j_mGVxCOvV578LjtZAWdZMDTDnI0TXyFKmxRR6na1/s500/Tawa-Kulcha.webp"
+        },
+        {
+          'id': 2,
+          'name': 'dal Makhani',
+          'price': 450,
+          'ratting': 3.89,
+          'discount': 12,
+          'thumbnail':
+              "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEid7JB3fKXpS18cDDajCW75TPNK2Us6yDLG4fM5Xagd3L6MA-fBYk7DdRKKS-I3KgvgkiTgYSt8_wpzFSkSW10asVWvh9rMIwqI09Og275dLuTNwc4MQW3qKcao88PJkrvHK-fxpFunL016GkRCqYuam8Zz0n92MEvx93ArQDDGlirRaF8VrtcYdeXy/s500/Dal-Makhani.webp",
+        },
+        {
+          'id': 3,
+          'name': 'Aloo Bhature',
+          'price': 250,
+          'ratting': 5,
+          'discount': 8,
+          'thumbnail':
+              "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgdp5NUX-U5gFSYOrK7sH4-niG0ruJb_zM-Ln2xXpROMzRn548bvWEh512vQXOdRPWquCGclufBeARoLdIekqdd14ZKCgfDImWJfh6sDQjx17fXrtbzrdKAOcodjYRpbs0AGH2nwsynL4Q1yt7otkOF0MtsX4L2IGR10KQZ9d93paVzkYHmvS9UlEm-/w320-h213/Aloo%20Bhatura.webp"
+        },
+        {
+          'id': 4,
+          'name': ' Palak Paneer',
+          'price': 300,
+          'ratting': 4.1,
+          'discount': 8,
+          'thumbnail':
+              "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgnEZrnlwUAB4vQTDojYzZYGtxgeZngX918tdy3Tam30ST6eaFa7htJumSGhwaYxG2ywDhzhBtVeOqfTDMgtLFKBZfh17Th7qLWfneRZK2fRnac7TxdZssZIghN_zTHOvpAP-AVPN3P6XZoIuwVVU1HqciT5e5_KX58mHVNkvCmH-jAH8zOcqS9vYSA/s500/Palak-Paneer.webp"
+        },
+        {
+          'id': 5,
+          'name': ' Makki ki Roti',
+          'price': 280,
+          'ratting': 4,
+          'discount': 12,
+          'thumbnail':
+              "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh2w9Hq3ViNV4NSOxm8e90uNIOFk9_mKJVawAiG8pRYOftSqWcR2o1Mg_vxHigtp0fl78R05lYN6y-SEVCdYNOUbQPOS-1fRW8VacxcQn6wg4WMFhsfsusVZTSaEwy4-ug-XlQn_4cXuGDBBHAdZX3GBSl1zQDvF8duGtHsYj8k-nWOD2kplkg6vUxx/s500/Maki-Ki-Roti.webp"
+        },
+      ],
+    },
+    {
+      'categoryName': 'Gujrati',
+      'categoryProducts': [
+        {
+          'id': 1,
+          'name': 'khaman Dhokla',
+          'price': 120,
+          'ratting': 4.8,
+          'discount': 14,
+          'thumbnail':
+              "https://www.holidify.com/images/cmsuploads/compressed/Dhokla_20180327130008.jpg",
+        },
+        {
+          'id': 2,
+          'name': 'Thepala',
+          'price': 150,
+          'ratting': 4.7,
+          'discount': 18,
+          'thumbnail':
+              "https://www.lacademie.com/wp-content/uploads/2022/04/gujarati-methi-thepla.jpg"
+        },
+        {
+          'id': 2,
+          'name': 'Undhiyu',
+          'price': 200,
+          'ratting': 4.1,
+          'discount': 20,
+          'thumbnail':
+              "https://www.storypick.com/wp-content/uploads/2015/01/11Undhiyu-recipe-gujarati-undhiyu-3.jpg"
+        },
+        {
+          'id': 4,
+          'name': 'patra',
+          'price': 180,
+          'ratting': 3.9,
+          'discount': 20,
+          'thumbnail':
+              "https://www.storypick.com/wp-content/uploads/2015/01/4alu-vadi.jpg"
+        },
+        {
+          'id': 5,
+          'name': 'Dal Dhokali',
+          'price': 230,
+          'ratting': 4,
+          'discount': 20,
+          'thumbnail':
+              "https://www.storypick.com/wp-content/uploads/2015/01/Dal-Dhokli-768x512.jpg"
+        },
+      ],
+    },
+    {
+      'categoryName': 'chinese',
+      'categoryProducts': [
+        {
+          'id': 1,
+          'name': 'Veg Manchurian',
+          'price': 230,
+          'ratting': 5,
+          'discount': 15,
+          'thumbnail' : "https://i.ndtvimg.com/i/2018-05/manchurian_620x330_81525941731.jpg"
+        },
+        {
+          'id': 2,
+          'name': 'chilli potato',
+          'price': 320,
+          'ratting': 4.5,
+          'discount': 18,
+          'thumbnail' : "https://i.ndtvimg.com/i/2018-05/honey-chili-potato_620x330_61525941818.jpg"
+        },
+        {
+          'id': 3,
+          'name': 'Spring Roll',
+          'price': 250,
+          'ratting': 4,
+          'discount': 15,
+          'thumbnail' : "https://i.ndtvimg.com/i/2015-10/spring-roll_625x350_81446202259.jpg"
+        },
+        {
+          'id': 4,
+          'name': ' Hakka Noodles',
+          'price': 360,
+          'ratting': 3.9,
+          'discount': 18,
+          'thumbnail' : "https://i.ndtvimg.com/i/2018-01/noodles-salad_620x350_61516262792.jpg"
+        },
+        {
+          'id': 5,
+          'name': 'chilli baby corn',
+          'price': 500,
+          'ratting': 4.2,
+          'discount': 15,
+          'thumbnail' : "https://c.ndtvimg.com/2021-10/pdd4ui4_chilli-baby-corn_625x300_08_October_21.jpg?im=FeatureCrop,algorithm=dnn,width=620,height=350"
+        },
+      ],
+    },
+    {
+      'categoryName': 'Fast Food',
+      'categoryProducts': [
+        {
+          'id': 1,
+          'name': 'Burger',
+          'price': 250,
+          'ratting': 5,
+          'discount': 15,
+          'thumbnail' : "https://images.pexels.com/photos/2725744/pexels-photo-2725744.jpeg?cs=srgb&dl=pexels-engin-akyurt-2725744.jpg&fm=jpg"
+        },
+        {
+          'id': 2,
+          'name': 'pizza',
+          'price': 650,
+          'ratting': 5,
+          'discount': 18,
+          'thumbnail' : "https://e1.pxfuel.com/desktop-wallpaper/646/123/desktop-wallpaper-olive-pizza-fast-food-food-lahmacun.jpg"
+        },
+        {
+          'id': 3,
+          'name': 'Sendwich',
+          'price': 300,
+          'ratting': 5,
+          'discount': 15,
+          'thumbnail' : "https://b.zmtcdn.com/data/pictures/8/18752848/92ca0f202b45aa623c90e9a4fed89c93.jpg"
+        },
+        {
+          'id': 4,
+          'name': 'Franch Frice',
+          'price': 200,
+          'ratting': 5,
+          'discount': 18,
+          'thumbnail' : "https://w7.pngwing.com/pngs/903/581/png-transparent-hd-fries-french-fries-fast-food-food.png"
+        },
+        {
+          'id': 5,
+          'name': 'Samosa',
+          'price': 150,
+          'ratting': 5,
+          'discount': 15,
+          'thumbnail' : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfM14CX79pmKVoaaHSmBv0AjnUkh_7H7aB6sWmSPZo2fBukKPfjutbAdDx38Dyrzv2MD4&usqp=CAU"
+        },
+      ],
+    },
+    {
+      'categoryName': 'Deserta',
+      'categoryProducts': [
+        {
+          'id': 1,
+          'name': 'choclate saik',
+          'price': 450,
+          'ratting': 5,
+          'discount': 15,
+          'thumbnail' : "https://images.unsplash.com/photo-1563805042-7684c019e1cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGVzc2VydHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+        },
+        {
+          'id': 2,
+          'name': 'chocolate pasty',
+          'price': 200,
+          'ratting': 5,
+          'discount': 18,
+          'thumbnail' : "https://c1.wallpaperflare.com/preview/733/148/987/chocolate-cake-chocolate-cake-recipe-chocolate-images-chocolate-ice-cream.jpg"
+        },
+        {
+          'id': 3,
+          'name': 'chocolate icecream',
+          'price': 300,
+          'ratting': 5,
+          'discount': 15,
+          'thumbnail' : "https://w0.peakpx.com/wallpaper/644/386/HD-wallpaper-chocolate-ice-cream-straws-dessert-sweets-ice-cream.jpg"
+        },
+        {
+          'id': 4,
+          'name': 'cake with ice cream',
+          'price': 400,
+          'ratting': 5,
+          'discount': 18,
+          'thumbnail' : "https://m.lovethispic.com/uploaded_images/71144-Chocolate-Cake-And-Ice-Cream.jpg"
+        },
+        {
+          'id': 5,
+          'name': 'sendwich cake',
+          'price': 600,
+          'ratting': 5,
+          'discount': 15,
+          'thumbnail' : "https://www.allrecipes.com/thmb/QPSjSOrAfUPaJzWlTTX5tE_ns18=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/232742-IceCreamSandwichCake-CD-2x1-9549-1-a8e9996492674eca9a2699e31420cbb9.jpg"
+        },
+      ],
+    },
+  ];
+}
